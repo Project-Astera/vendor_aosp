d1e8c201f25aacea8a89e2129d66d40d4b4a8a22
MrErenK <akbaseren4751@gmail.com>
2024-02-01 15:26:01 +0000

    vendorsetup: Drop running official devices script * Already running on build_make
    Signed-off-by: MrErenK <akbaseren4751@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,3 @@
-lunch_others_targets=()
-for d in $(python vendor/aosp/tools/get_official_devices.py)
-do
-    for v in user userdebug eng; do
-        lunch_others_targets+=("aosp_$d-$v")
-    done
-done
-
 # Shebang is intentionally missing - do not run as a script
 
 # ABI compatibility checks fail for several reasons:
